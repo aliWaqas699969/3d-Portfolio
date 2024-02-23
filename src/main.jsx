81c2aee049f30d28f00d762fd0266791806c35d7
Ali waqas <aliwaqas55488@gmail.com>
2024-02-23 16:37:15 +0500

    Speed Change

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SpeedInsights>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </SpeedInsights>
+);
